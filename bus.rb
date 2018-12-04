c10976f72f578061491a603ae586bb6af417b5a5
user <user@users-MacBook-Pro.local>
2018-12-04 13:43:13 +0000

    Setup of class and test class

---
+++ bus.rb
@@ -0,0 +1,12 @@
+class Bus
+
+  attr_reader :route_no, :destination
+
+  def initialize(route_no, destination)
+    @route_no = route_no
+    @destination = destination
+  end
+
+
+
+end
---
+++ specs/bus_spec.rb
@@ -0,0 +1,13 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../bus')
+
+class BusTest < MiniTest::Test
+
+  def setup
+    @bus = Bus.new("22", "Ocean Terminal")
+
+  end
+
+
+end
